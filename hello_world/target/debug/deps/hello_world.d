ce73af1775df7edf73e6240a9f30cc3256474d9d
Benedikt Radtke <benediktradtke@gmail.com>
2020-03-22 21:14:55 +0100

    remove useless

--- hello_world/target/debug/deps/hello_world.d
+++
@@ -1,6 +0,0 @@
-/home/benni/repositories/EnoMoloch/hello_world/target/debug/deps/libhello_world.so: src/lib.rs src/ffi.rs
-
-/home/benni/repositories/EnoMoloch/hello_world/target/debug/deps/hello_world.d: src/lib.rs src/ffi.rs
-
-src/lib.rs:
-src/ffi.rs:
--- hello_world/target/debug/libhello_world.d
+++
@@ -1,1 +0,0 @@
-/home/benni/repositories/EnoMoloch/hello_world/target/debug/libhello_world.so: /home/benni/repositories/EnoMoloch/hello_world/src/ffi.rs /home/benni/repositories/EnoMoloch/hello_world/src/lib.rs
