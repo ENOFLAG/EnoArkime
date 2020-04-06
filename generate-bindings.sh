9dfd7b268a1814cf09466c84689091f23ea09fab
Lucas Druschke <ldruschk@posteo.de>
2020-04-06 15:10:46 +0200

    make generate-binding.sh work on Arch

--- generate-bindings.sh
+++ generate-bindings.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-bindgen --size_t-is-usize moloch.h -o tmp.rs -- -Ithirdparty -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include/
+bindgen --size_t-is-usize moloch.h -o tmp.rs -- -Ithirdparty -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include/ -I/usr/lib/glib-2.0/include/
 cat << EOF > bindings.rs
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
