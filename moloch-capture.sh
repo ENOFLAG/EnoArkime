5fd03965d9e20f1a3fbdcad2ee17b6eaa0d265ea
Lucas Druschke <ldruschk@posteo.de>
2020-04-29 22:09:04 +0200

    specify node names for viewer/capture

--- moloch-capture.sh
+++ moloch-capture.sh
@@ -3,6 +3,6 @@
 while :
 do
   echo "Starting Moloch capture"
-  ./bin/moloch-capture -R /data/moloch/raw -m
+  ./bin/moloch-capture -n docker -R /data/moloch/raw -m
   sleep 5
 done
--- moloch-viewer.sh
+++ moloch-viewer.sh
@@ -4,6 +4,6 @@ cd viewer
 while :
 do
   echo "Starting Moloch viewer"
-  ../bin/node ./viewer.js
+  ../bin/node ./viewer.js -n docker
   sleep 5
 done
