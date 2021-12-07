34b270803cd672afd7ae67a558b4e55e2c46964a
Benedikt Radtke <benediktradtke@gmail.com>
2021-12-07 23:37:06 +0100

    properly check curl reponse

--- elasticsearch_init.sh
+++ elasticsearch_init.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 while ! curl -sq http://elasticsearch:9200; do
-        echo "Waiting for elastic search to start...";
-        sleep 3;
+    echo "Waiting for elastic search to start...";
+    sleep 3;
 done
 
 echo "Check if elasticsearch is initalized, otherwise do it"
-if [[ ! $(curl -s --head "http://elasticsearch:9200/arkime_dstats_v30") == *"200*" ]] && \
-   [[ ! $(curl -s --head "http://elasticsearch:9200/arkime_users_v30") == *"200*" ]] && \
-   [[ ! $(curl -s --head "http://elasticsearch:9200/arkime_fields_v30") == *"200*" ]] && \
-   [[ ! $(curl -s --head "http://elasticsearch:9200/arkime_queries_v30") == *"200*" ]] && \
-   [[ ! $(curl -s --head "http://elasticsearch:9200/arkime_stats_v30") == *"200*" ]] && \
-   [[ ! $(curl -s --head "http://elasticsearch:9200/arkime_sequence_v30") == *"200*" ]] && \
-   [[ ! $(curl -s --head "http://elasticsearch:9200/arkime_hunts_v30") == *"200*" ]] && \
-   [[ ! $(curl -s --head "http://elasticsearch:9200/arkime_files_v30") == *"200*" ]] && \
-   [[ ! $(curl -s --head "http://elasticsearch:9200/arkime_lookups_v30") == *"200*" ]]; then
+if [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_dstats_v30") ]] && \
+   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_users_v30") ]] && \
+   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_fields_v30") ]] && \
+   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_queries_v30") ]] && \
+   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_stats_v30") ]] && \
+   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_sequence_v30") ]] && \
+   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_hunts_v30") ]] && \
+   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_files_v30") ]] && \
+   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_lookups_v30") ]]; then
    echo "Initializing elasticsearch..."
    (echo "INIT" | /opt/arkime/db/db.pl http://elasticsearch:9200 init) || exit 1
 
