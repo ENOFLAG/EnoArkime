524424303a7751b8be2fdbf48f18726dabbd7501
ldruschk <14059613+ldruschk@users.noreply.github.com>
2023-09-17 15:18:51 +0200

    update to arkime 4.5.0 (#11)
    * update to arkime 4.5.0
    
    * add GitHub Actions workflow for docker build on PR
    
    * fix workflow name, bump checkout action to v4

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,16 @@
-FROM ubuntu:20.04
+FROM ubuntu:22.04
 
-ENV VERSION=3.1.1
+ENV VERSION=4.5.0
 
-ADD https://s3.amazonaws.com/files.molo.ch/builds/ubuntu-20.04/arkime_$VERSION-1_amd64.deb .
-RUN apt-get update && \
-    apt-get install -y htop nano tree curl libwww-perl libjson-perl ethtool libyaml-dev && \
-    dpkg -i arkime_$VERSION-1_amd64.deb && \
-    apt-get install -y libmagic-dev && \
+ADD https://s3.amazonaws.com/files.molo.ch/builds/ubuntu-22.04/arkime_${VERSION}-1_amd64.deb .
+RUN apt update && \
+    apt install -y htop nano tree curl libwww-perl libjson-perl ethtool libyaml-dev liblua5.4-0 libmaxminddb0 libcurl4 libpcap0.8 libglib2.0-0 libnghttp2-14 libyara8 librdkafka1 && \
+    dpkg -i arkime_${VERSION}-1_amd64.deb && \
+    apt install -y libmagic-dev && \
     rm -rf arkime_$VERSION-1_amd64.deb && \
     rm -rf /var/lib/apt/lists/*
 
-RUN curl https://raw.githubusercontent.com/wireshark/wireshark/master/manuf > /opt/arkime/etc/oui.txt
+RUN curl https://fossies.org/linux/misc/wireshark-4.0.8.tar.xz/wireshark-4.0.8/manuf?m=b > /opt/arkime/etc/oui.txt
 RUN curl https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv > /opt/arkime/etc/ipv4-address-space.csv
 
 COPY elasticsearch_init.sh elasticsearch_init.sh
--- elasticsearch_init.sh
+++ elasticsearch_init.sh
@@ -6,18 +6,20 @@ while ! curl -sq http://elasticsearch:9200; do
 done
 
 echo "Check if elasticsearch is initalized, otherwise do it"
-if [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_dstats_v30") ]] && \
-   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_users_v30") ]] && \
-   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_fields_v30") ]] && \
-   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_queries_v30") ]] && \
-   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_stats_v30") ]] && \
-   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_sequence_v30") ]] && \
-   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_hunts_v30") ]] && \
-   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_files_v30") ]] && \
-   [[ ! $(curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_lookups_v30") ]]; then
+if ! curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_dstats_v30" && \
+   ! curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_users_v30" && \
+   ! curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_fields_v30" && \
+   ! curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_queries_v30" && \
+   ! curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_stats_v30" && \
+   ! curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_sequence_v30" && \
+   ! curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_hunts_v30" && \
+   ! curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_files_v30" && \
+   ! curl -s --head --show-error --fail "http://elasticsearch:9200/arkime_lookups_v30"; then
    echo "Initializing elasticsearch..."
    (echo "INIT" | /opt/arkime/db/db.pl http://elasticsearch:9200 init) || exit 1
 
    echo "Adding Arkime user"
    /opt/arkime/bin/arkime_add_user.sh "arkime" "Arkime User" "arkime" --admin --packetSearch || exit 1
+else
+   echo "elasticsearch was already initalized, so initialization was skipped"
 fi
