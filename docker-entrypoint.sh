58b9f480344616308ceebb11159f37dd4c97eb50
Benedikt Radtke <benediktradtke@gmail.com>
2024-04-06 11:43:56 +0200

    5.1.0

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-bash /elasticsearch_init.sh || exit 1
-sh /arkime-capture.sh &
-sh /arkime-viewer.sh &
+bash ./elasticsearch_init.sh || exit 1
+sh ./arkime-capture.sh &
+sh ./arkime-viewer.sh &
 wait
